c88f7bcc747a2f04ac06cd0f30a378fb04b0c074
juanjitomp2005-hash <124716825+juanjomon05@users.noreply.github.com>
2026-02-08 20:33:58 -0500

    Busqueda de peliculas
    Se agrego la lista de peliculas y un buscador para filtrar las peliculas

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movies
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name':'Juaose Montoya'})
+   searchTem =  request.GET.get('searchMovie')
+   if searchTem:
+       movies = Movies.objects.filter(title__icontains=searchTem)
+   else: 
+       movies = Movies.objects.all
+   return render(request, 'home.html', {'searchTerm': searchTem, 'movies': movies})
+
 
 def about(request):
     return render(request, 'about.html')
