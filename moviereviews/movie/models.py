75b8feeab493bd8ebef7cecc809b12effeee2668
juanjitomp2005-hash <124716825+juanjomon05@users.noreply.github.com>
2026-02-08 20:57:01 -0500

    Vsiaulizacion de peliculas con estilo

--- moviereviews/movie/models.py
+++ moviereviews/movie/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Movie(models.model):
-    tittle = models.charlfield(max_length=100)
+class Movies(models.Model):
+    title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images')
     url = models.URLField(blank =True)
